bb4a5a12c03b8b7cacf1a8925f1c2e941003f72b
jair <129990475+jairwictor@users.noreply.github.com>
2023-09-21 19:27:32 -0300

    Add files via upload

---
+++ segunda_atividade/segunda_atividade.js
@@ -0,0 +1,6 @@
+var nota1 = prompt('Digite a primeira nota:');
+var nota2 = prompt('Digite a segunda nota:');
+var nota3 = prompt('Digite a terceira nota:');
+
+var media_ponderada = ((nota1 * 0.2) + (nota2 * 0.3) + (nota3 * 0.5));
+alert(media_ponderada);
